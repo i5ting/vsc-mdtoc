28da243076729ae8f8c28714173c7c6f9c11372d
sangshilong <sangshilong@luojilab.com>
2017-09-08 08:49:06 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -2,6 +2,60 @@
 
 var a = require("markdown-it-toc");
 
+/*! ztree_toc - v0.2.2 - 2014-02-08
+* https://github.com/i5ting/jQuery.zTree_Toc.js
+* Copyright (c) 2014 alfred.sang; Licensed MIT */
+function encode_id_with_array(opts,arr) {
+	var result = 0;
+  	for(var z = 0; z < arr.length; z++ ) {  
+		result += factor(opts, arr.length - z ,arr[z]);
+  	}
+
+	return result;
+}
+
+
+/**
+ * 1.1.1 = 1*100*100 + 1*100 + 1
+ * 1.2.2 = 1*100*100 + 2*100 + 3
+ *
+ * 1 = 0*100 +1
+
+	1,1 = 100
+
+ */ 
+function get_parent_id_with_array(opts,arr) {
+	var result_arr = [];
+
+  	for(var z = 0; z < arr.length; z++ ) {  
+		result_arr.push(arr[z]);
+  	}
+	
+	result_arr.pop();
+	
+	var result = 0;
+  	for(var z = 0; z < result_arr.length; z++ ) {  
+		result += factor(opts,result_arr.length - z,result_arr[z]);
+  	}
+	
+	return result;
+}
+
+function factor(opts ,count,current) {
+	if(1 == count) {
+		return current;
+	}
+	
+	var str = '';
+	for(var i = count - 1;i > 0; i-- ) {
+		str += current * opts.step+'*';
+	}
+
+	return eval( str + '1' );
+}
+
+
+
 function permalink (md) {
   var originalHeadingOpen = md.renderer.rules.heading_open
 
@@ -12,9 +66,34 @@ function permalink (md) {
     const anchor = tokens[idx + 1]._tocAnchor
     attrs.push([ "id", anchor ])
 
-    if (options.anchorLink) {
-    //   renderAnchorLink(anchor, options, ...args)
+    var rule = tokens[idx];
+    
+    if (rule.type == 'heading_open'){
+        var level = parseInt(rule.tag.replace('h' , ''))
+
+        console.log(level)
+
+        var title = tokens[idx + 1]
+
+        console.log(title.content)
+        console.log(title.level)
     }
+
+    // var id  = encode_id_with_array(opts,opts._headers);
+    // var pid = get_parent_id_with_array(opts,opts._headers);
+
+    // opts._header_nodes.push({
+    //     id:id, 
+    //     pId:pid , 
+    //     name:$(header_obj).text()||'null', 
+    //     open:true,
+    //     url:'#'+ id,
+    //     target:'_self'
+    // });
+
+    // if (options.anchorLink) {
+    //   renderAnchorLink(anchor, options, ...args)
+    // }
 
     return originalHeadingOpen.apply(this, args)
   }
