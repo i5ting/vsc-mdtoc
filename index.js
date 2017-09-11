5ee81f6e905df98fa7bf4dd4df0f068d2acc0fe7
sangshilong <sangshilong@luojilab.com>
2017-09-11 10:19:05 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var a = require("markdown-it-toc");
 /*! ztree_toc - v0.2.2 - 2014-02-08
 * https://github.com/i5ting/jQuery.zTree_Toc.js
 * Copyright (c) 2014 alfred.sang; Licensed MIT */
-function encode_id_with_array(opts,arr) {
-	var result = 0;
-  	for(var z = 0; z < arr.length; z++ ) {  
-		result += factor(opts, arr.length - z ,arr[z]);
-  	}
+function encode_id_with_array(opts, arr) {
+    var result = 0;
+    for (var z = 0; z < arr.length; z++) {
+        result += factor(opts, arr.length - z, arr[z]);
+    }
 
-	return result;
+    return result;
 }
 
 
@@ -23,80 +23,127 @@ function encode_id_with_array(opts,arr) {
 
 	1,1 = 100
 
- */ 
-function get_parent_id_with_array(opts,arr) {
-	var result_arr = [];
+ */
+function get_parent_id_with_array(opts, arr) {
+    var result_arr = [];
 
-  	for(var z = 0; z < arr.length; z++ ) {  
-		result_arr.push(arr[z]);
-  	}
-	
-	result_arr.pop();
-	
-	var result = 0;
-  	for(var z = 0; z < result_arr.length; z++ ) {  
-		result += factor(opts,result_arr.length - z,result_arr[z]);
-  	}
-	
-	return result;
-}
-
-function factor(opts ,count,current) {
-	if(1 == count) {
-		return current;
-	}
-	
-	var str = '';
-	for(var i = count - 1;i > 0; i-- ) {
-		str += current * opts.step+'*';
-	}
-
-	return eval( str + '1' );
-}
-
-
-
-function permalink (md) {
-  var originalHeadingOpen = md.renderer.rules.heading_open
-
-  md.renderer.rules.heading_open =   md.renderer.rules.heading_open = function(...args) {
-    const [ tokens, idx, , , ] = args
-
-    const attrs = tokens[idx].attrs = tokens[idx].attrs || []
-    const anchor = tokens[idx + 1]._tocAnchor
-    attrs.push([ "id", anchor ])
-
-    var rule = tokens[idx];
-    
-    if (rule.type == 'heading_open'){
-        var level = parseInt(rule.tag.replace('h' , ''))
-
-        console.log(level)
-
-        var title = tokens[idx + 1]
-
-        console.log(title.content)
-        console.log(title.level)
+    for (var z = 0; z < arr.length; z++) {
+        result_arr.push(arr[z]);
     }
 
-    // var id  = encode_id_with_array(opts,opts._headers);
-    // var pid = get_parent_id_with_array(opts,opts._headers);
+    result_arr.pop();
 
-    // opts._header_nodes.push({
-    //     id:id, 
-    //     pId:pid , 
-    //     name:$(header_obj).text()||'null', 
-    //     open:true,
-    //     url:'#'+ id,
-    //     target:'_self'
-    // });
+    var result = 0;
+    for (var z = 0; z < result_arr.length; z++) {
+        result += factor(opts, result_arr.length - z, result_arr[z]);
+    }
 
-    // if (options.anchorLink) {
-    //   renderAnchorLink(anchor, options, ...args)
-    // }
+    return result;
+}
 
-    return originalHeadingOpen.apply(this, args)
-  }
+function factor(opts, count, current) {
+    if (1 == count) {
+        return current;
+    }
+
+    var str = '';
+    for (var i = count - 1; i > 0; i--) {
+        str += current * opts.step + '*';
+    }
+
+    return eval(str + '1');
+}
+
+
+
+var opts = {
+    _headers:[]
+}
+/*
+ * 将已有header编号，并重命名
+ */
+function _rename_header_content(opts, header_obj, level) {
+    if (opts._headers.length == level) {
+        opts._headers[level - 1]++;
+    } else if (opts._headers.length > level) {
+        opts._headers = opts._headers.slice(0, level);
+        opts._headers[level - 1]++;
+    } else if (opts._headers.length < level) {
+        for (var i = 0; i < (level - opts._headers.length); i++) {
+            // console.log('push 1');
+            opts._headers.push(1);
+        }
+    }
+
+    if (opts.is_auto_number == true) {
+        //另存为的文件里会有编号，所以有编号的就不再重新替换
+        // if ($(header_obj).text().indexOf(opts._headers.join('.')) != -1) {
+
+        // } else {
+        //     $(header_obj).text(opts._headers.join('.') + '. ' + $(header_obj).text());
+        // }
+    }
+}
+
+
+
+function permalink(md) {
+    var originalHeadingOpen = md.renderer.rules.heading_open
+
+    md.renderer.rules.heading_open = md.renderer.rules.heading_open = function (...args) {
+        const [tokens, idx, , ,] = args
+
+        const attrs = tokens[idx].attrs = tokens[idx].attrs || []
+        const anchor = tokens[idx + 1]._tocAnchor
+        attrs.push(["id", anchor])
+
+        var rule = tokens[idx];
+
+        if (rule.type == 'heading_open') {
+            var level = parseInt(rule.tag.replace('h', ''))
+            var markup = rule.markup
+
+            console.log(level)
+
+            var title = tokens[idx + 1]
+
+
+            
+            console.log(title.content)
+            // console.log(title.level)
+
+            var header_obj = [rule.tag + markup +  title.content]
+            _rename_header_content(opts, header_obj, level);
+
+            var id  = encode_id_with_array(opts,opts._headers);
+            var pid = get_parent_id_with_array(opts,opts._headers);
+    
+            console.log(level + ' = level')
+            console.log(id+ ' = id')
+            console.log(pid+ ' = pid')
+
+            var obj = {
+
+            }
+        }
+
+        
+
+        // opts._header_nodes.push({
+        //     id:id, 
+        //     pId:pid , 
+        //     name:$(header_obj).text()||'null', 
+        //     open:true,
+        //     url:'#'+ id,
+        //     target:'_self'
+        // });
+
+        // if (options.anchorLink) {
+        //   renderAnchorLink(anchor, options, ...args)
+        // }
+
+        return originalHeadingOpen.apply(this, args)
+    }
 
 
 }
